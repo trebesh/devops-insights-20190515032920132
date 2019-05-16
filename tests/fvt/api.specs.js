0c9b2de018e81e4da959267759a72687489c1769
Hannah Trebes <trebeshm@gmail.com>
2019-05-16 21:05:28 +0000

    FVT test 1 attempt 1
    Previously: unit tests passed

--- tests/fvt/api.specs.js
+++ tests/fvt/api.specs.js
@@ -15,7 +15,7 @@
 
     describe('Get Weather', function() {
     	it('pass', function(done){done()})
-/*
+
     	it('with valid city name', function(done) {
         if(!appUrl) {
             assert.fail("Environment variable APP_URL is not defined");
@@ -23,7 +23,7 @@
         }
         request({
       		method: 'GET',
-              url: appUrl + '/api/v1/getWeather?q=London'
+              url: appUrl + '/api/v1/getWeather?q=Hamilton'
           }, function(err, resp, body) {
           	if(err) {
           		assert.fail('Failed to get the response');
@@ -34,7 +34,7 @@
             }
         });
     	});
-
+/*
       it('without city name', function(done) {
         if(!appUrl) {
             assert.fail("Environment variable APP_URL is not defined");
