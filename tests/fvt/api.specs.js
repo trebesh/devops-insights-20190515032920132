4a617dd7c31286984d05f4704850df27fb15ce77
Hannah Trebes <trebeshm@gmail.com>
2019-05-16 21:49:26 +0000

    Manual revert to initial FVT

--- tests/fvt/api.specs.js
+++ tests/fvt/api.specs.js
@@ -1,5 +1,73 @@
-
 (function () {
+    'use strict';
+    var apiv1 = require('../../routes/apiv1');
+    var assert = require('chai').assert;
+    var REQUEST = require('request');
+ var request = REQUEST.defaults( {
+        strictSSL: false
+    });
+    var appUrl = process.env.APP_URL;
+    describe('Get Weather', function() {
+    	it('with valid zip code', function(done) {
+        if(!appUrl) {
+            assert.fail("Environment variable APP_URL is not defined");
+            return done();
+        }
+        request({
+      		method: 'GET',
+              url: appUrl + '/api/v1/getWeather?zip=78613'
+          }, function(err, resp, body) {
+          	if(err) {
+          		assert.fail('Failed to get the response');
+          	} else {
+              assert.equal(resp.statusCode, 200);
+              var pbody = JSON.parse(body);
+              assert((pbody.city === 'Anderson Mill') || (pbody.city === 'Round Rock'), "City name does not match");
+              done();
+            }
+        });
+    	});
+      it('without zip code', function(done) {
+        if(!appUrl) {
+            assert.fail("Environment variable APP_URL is not defined");
+            return done();
+        }
+        request({
+      		method: 'GET',
+              url: appUrl + '/api/v1/getWeather'
+          }, /* @callback */ function(err, resp, body) {
+          	if(err) {
+          		assert.fail('Failed to get the response');
+          	} else {
+              assert.equal(resp.statusCode, 400);
+              done();
+            }
+        });
+    	});
+      it('with another valid zip code', function(done) {
+        if(!appUrl) {
+            assert.fail("Environment variable APP_URL is not defined");
+            return done();
+        }
+        request({
+      		method: 'GET',
+              url: appUrl + '/api/v1/getWeather?zip=78641'
+          }, function(err, resp, body) {
+          	if(err) {
+          		assert.fail('Failed to get the response');
+          	} else {
+              assert.equal(resp.statusCode, 200);
+              var pbody = JSON.parse(body);
+              assert(pbody.city === 'Round Rock', "City name does not match");
+              done();
+            }
+        });
+    	});
+    });
+})();
+
+
+/*(function () {
 
     'use strict';
 
@@ -43,7 +111,7 @@
         request({
       		method: 'GET',
               url: appUrl + '/api/v1/getWeather'
-          }, /* @callback */ function(err, resp, body) {
+          }, /* @callback */ /* function(err, resp, body) {
           	if(err) {
           		assert.fail('Failed to get the response');
           	} else {
@@ -75,3 +143,4 @@
     	
     });
 })();
+*/
