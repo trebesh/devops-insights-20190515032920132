1fd089fbb299bf7519e3bc2b71cc3f6fa5250d94
Hannah Trebes <trebeshm@gmail.com>
2019-05-16 03:54:09 +0000

    attempting changing value to metric but not 'F' to 'C'

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -29,7 +29,7 @@ exports.getWeather = function(req, res) {
     		//console.error("Failed to send request to openweathermap.org", err);
     	} else {
     		if(body.cod === 200) {
-    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' C';
+    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' F';
 
     			var response = {city: body.name, weather: weath};
     			return res.status(200).send(response);
@@ -61,7 +61,7 @@ exports.getWeather2 = function(req, res) {
     		//console.error("Failed to send request to openweathermap.org", err);
     	} else {
     		if(body.cod === 200) {
-    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' C';
+    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' F';
     			var response = {city: body.name, weather: weath};
     			return res.status(200).send(response);
     		} else {
