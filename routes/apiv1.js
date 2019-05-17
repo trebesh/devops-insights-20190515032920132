5f033b7bce8dac553a4a04b764ca7d2de04bc378
Hannah Trebes <trebeshm@gmail.com>
2019-05-17 01:33:23 +0000

    Added FVT tests for: invalid city name London Incomplete name
    Hard delete getWeather2

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -7,7 +7,6 @@ var request = REQUEST.defaults( {
     strictSSL: false
 });
 
-//var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial";
 var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric"; 
 
 
@@ -42,38 +41,5 @@ exports.getWeather = function(req, res) {
 
 };
 router.get('/getWeather', exports.getWeather);
-
-
-/*exports.getWeather2 = function(req, res) {
-	var city = req.query.city;
-	if( (city === null) || (typeof(city) === 'undefined') ) {
-		return res.status(400).send('city missing');
-	}
-
-	var aurl = OPENWEATHERURL + '&q=' + city+ ',nz';
-
-	request({
-		method: 'GET',
-        url: aurl,
-  		json: true
-    }, function(err, resp, body) {
-    	if(err) {
-    		res.status(400).send('Failed to get the data');
-    		console.error("Failed to send request to openweathermap.org", err);
-    	} else {
-    		if(body.cod === 200) {
-    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' C';
-    			var response = {city: body.name, weather: weath};
-    			return res.status(200).send(response);
-    		} else {
-    			console.error("Failed to get response", err);    			
-                return res.status(400).send({msg:'Failed'});
-            }
-    	}
-    });
-
-};
-router.get('/getWeather2', exports.getWeather2);*/
-
 
 exports.router = router;
--- tests/fvt/api.specs.js
+++ tests/fvt/api.specs.js
@@ -66,6 +66,42 @@
             }
         });
     	});
+    	it('with invalid city name', function(done) {
+        if(!appUrl) {
+            assert.fail("Environment variable APP_URL is not defined");
+            return done();
+        }
+        request({
+      		method: 'GET',
+              url: appUrl + '/api/v1/getWeather?city=London'
+          }, function(err, resp, body) {
+          	if(err) {
+          		assert.fail('Failed to get the response');
+          	} else {
+              assert.equal(resp.statusCode, 400);
+              var pbody = JSON.parse(body);
+              done();
+            }
+        });
+    	});
+    	it('with incomplete city name', function(done) {
+        if(!appUrl) {
+            assert.fail("Environment variable APP_URL is not defined");
+            return done();
+        }
+        request({
+      		method: 'GET',
+              url: appUrl + '/api/v1/getWeather?city=Auckla,nz'
+          }, function(err, resp, body) {
+          	if(err) {
+          		assert.fail('Failed to get the response');
+          	} else {
+              assert.equal(resp.statusCode, 400);
+              var pbody = JSON.parse(body);
+              done();
+            }
+        });
+    	});
     	
     });
 })();
